2429ac6a24b6a8ea3a099348755e0534622c4844
BlusterySasha <52814854+BlusterySasha-SoulMine@users.noreply.github.com>
2019-09-08 17:24:37 +0400

    Update Main.php

--- src/BlusterySasha/LimitGM/Main.php
+++ src/BlusterySasha/LimitGM/Main.php
@@ -6,6 +6,7 @@ use pocketmine\Player;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
 use pocketmine\block\Block;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerDropItemEvent;
