7c61896e33ef26ab9050c3154552773fbbf0a2cc
BlusterySasha <52814854+BlusterySasha-SoulMine@users.noreply.github.com>
2019-09-10 18:47:07 +0400

    New update.
    Information: bug fix and load fix.

--- src/BlusterySasha/LimitGM/Main.php
+++ src/BlusterySasha/LimitGM/Main.php
@@ -34,6 +34,13 @@ class Main extends PluginBase implements Listener{
     }
 
 
+    function onLoad( )
+	{
+		UpdateNotifier::checkUpdate($this, $this->getName(), $this->getDescription()->getVersion());
+		$this->loadInstance();
+	}
+
+
     public function onGameModeChange(PlayerGameModeChangeEvent $event){
         $player = $event->getPlayer();
         $newGM = $event->getNewGamemode();
