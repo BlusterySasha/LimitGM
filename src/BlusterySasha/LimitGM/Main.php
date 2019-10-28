3418796d7301f2eab238e7518de9e0cf38347e48
BlusterySasha <52814854+BlusterySasha-SoulMine@users.noreply.github.com>
2019-10-28 18:29:52 +0400

    Bug fix.

--- src/BlusterySasha/LimitGM/Main.php
+++ src/BlusterySasha/LimitGM/Main.php
@@ -27,7 +27,7 @@ class Main extends PluginBase implements Listener{
         $blacklist = [Block::ENCHANTMENT_TABLE, Block::ANVIL, Block::ITEM_FRAME_BLOCK, Block::SHULKER_BOX, Block::TNT, Block::DIAMOND_BLOCK, Block::IRON_BLOCK, Block::LAPIS_BLOCK, Block::EMERALD_BLOCK, Block::COAL_BLOCK, Block::UNDYED_SHULKER_BOX, Block::DIAMOND_ORE, Block::QUARTZ_ORE, Block::EMERALD_ORE, Block::COAL_ORE, Block::REDSTONE_ORE, Block::LAPIS_ORE, Block::IRON_ORE, Block::GOLD_ORE, Block::GOLD_BLOCK, Block::OBSIDIAN, Block::BEDROCK, Block::END_PORTAL_FRAME, Block::PISTON, Block::STICKY_PISTON];
         if ($player->isCreative()){
             if (in_array($blocks, $blacklist)){
-				$player->sendTip("§l§7You cannot put this block while you are in the creative!");
+				$player->sendTip("§l§7You cannot put this block while you are in the creative.");
 				$player->addTitle("§e§lProtected§7!", " ");
                 $event->setCancelled();
                 $pk = new PlaySoundPacket();
@@ -49,7 +49,7 @@ class Main extends PluginBase implements Listener{
         if ($newGM === 0){
             $player->getInventory()->clearAll();
             $player->getArmorInventory()->clearAll();
-	        $player->sendTip("§l§7Your items have been cleaned!");
+	        $player->sendTip("§l§7Your items have been cleaned.");
 			$player->addTitle("§e§lProtected§7!", " ");
 			$pk = new PlaySoundPacket();
             $pk->x = $player->getX();
@@ -67,7 +67,7 @@ class Main extends PluginBase implements Listener{
         $player = $event->getPlayer();
         $newGM = $event->getNewGamemode();
         if ($newGM === 1){
-	        $player->sendTip("§l§7Upon transition to survival, things will be cleared!");
+	        $player->sendTip("§l§7Upon transition to survival, things will be cleared.");
 			$player->addTitle("§c§lWarning§7!", " ");
             return;
         }
@@ -79,7 +79,7 @@ class Main extends PluginBase implements Listener{
         $blacklist = [Block::ENDER_CHEST, Block::CRAFTING_TABLE, Block::CHEST, Block::FURNACE, Block::BURNING_FURNACE, Block::TRAPPED_CHEST, Block::ENCHANTMENT_TABLE, Block::ANVIL, Block::ITEM_FRAME_BLOCK, Block::SHULKER_BOX, Block::TNT, Block::DROPPER, Block::DISPENSER, Block::UNDYED_SHULKER_BOX];
         if ($player->isCreative()){
             if (in_array($blocks, $blacklist)){
-				$player->sendTip("§l§7You cannot touch this block in creative!");
+				$player->sendTip("§l§7You cannot touch this block in creative.");
 				$player->addTitle("§e§lProtected§7!", " ");
 				$pk = new PlaySoundPacket();
 				$pk->x = $player->getX();
@@ -100,7 +100,7 @@ class Main extends PluginBase implements Listener{
         if ($player->isCreative()){
             $player->getInventory()->clearAll();
             $player->getArmorInventory()->clearAll();
-			$player->sendTip("§l§7You died in the creative, items did not fall out!");
+			$player->sendTip("§l§7You died in the creative, items did not fall out.");
 			$player->addTitle("§e§lProtected§7!", " ");
 			$pk = new PlaySoundPacket();
             $pk->x = $player->getX();
@@ -117,7 +117,7 @@ class Main extends PluginBase implements Listener{
     {
         $player = $event->getPlayer();
         if ($player->isCreative()){
-	        $player->sendTip("§l§7You cannot throw items in creative!");
+	        $player->sendTip("§l§7You cannot throw items in creative.");
 			$player->addTitle("§e§lProtected§7!", " ");
 			$pk = new PlaySoundPacket();
             $pk->x = $player->getX();
@@ -136,7 +136,7 @@ class Main extends PluginBase implements Listener{
             $player = $event->getDamager();
             if ($player instanceof Player){
                 if ($player->isCreative()) {
-					$player->sendTip("§l§7You cannot beat in creative!");
+					$player->sendTip("§l§7You cannot beat in creative.");
 					$player->addTitle("§e§lProtected§7!", " ");
 					$pk = new PlaySoundPacket();
 					$pk->x = $player->getX();
